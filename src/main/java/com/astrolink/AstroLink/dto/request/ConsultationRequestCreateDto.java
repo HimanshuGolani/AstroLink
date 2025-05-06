c1f44e12645990d220e5baa9906c970a9f757adf
Himanshu Golani <121915939+HimanshuGolani@users.noreply.github.com>
2025-05-06 17:52:50 +0530

    Update ConsultationRequestCreateDto.java

--- src/main/java/com/astrolink/AstroLink/dto/request/ConsultationRequestCreateDto.java
+++ src/main/java/com/astrolink/AstroLink/dto/request/ConsultationRequestCreateDto.java
@@ -1,5 +1,6 @@
 package com.astrolink.AstroLink.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ConsultationRequestCreateDto {
+    @NotNull(message = "The title cannot be null")
     private String title;
     private String birthDate;
     private String birthTime;
